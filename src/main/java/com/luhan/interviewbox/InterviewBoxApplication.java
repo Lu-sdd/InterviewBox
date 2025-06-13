57b4b2273d419304bed2fb5bf89b2f1e68184067
Lu <l.han991230@gmail.com>
2025-06-13 14:11:17 +0100

    Initial commit

---
+++ src/main/java/com/luhan/interviewbox/InterviewBoxApplication.java
@@ -0,0 +1,13 @@
+package com.luhan.interviewbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewBoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewBoxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luhan/interviewbox/InterviewBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luhan.interviewbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewBoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
